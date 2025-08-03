be8fc8131da9b5194217018bf5449764194e17ee
khashayar <aripastudio44@gmail.com>
2025-08-02 22:27:56 -0700

    Complete UI

--- DockDAP/dmeAPControl.xaml.cs
+++ DockDAP/dmeAPControl.xaml.cs
@@ -9,19 +9,11 @@ namespace DockDAP
     /// </summary>
     public partial class dmeAPControl : UserControl
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="dmeAPControl"/> class.
-        /// </summary>
         public dmeAPControl()
         {
             this.InitializeComponent();
         }
 
-        /// <summary>
-        /// Handles click on the button by displaying a message box.
-        /// </summary>
-        /// <param name="sender">The event sender.</param>
-        /// <param name="e">The event args.</param>
         [SuppressMessage("Microsoft.Globalization", "CA1300:SpecifyMessageBoxOptions", Justification = "Sample code")]
         [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Default event handler naming pattern")]
         private void button1_Click(object sender, RoutedEventArgs e)
